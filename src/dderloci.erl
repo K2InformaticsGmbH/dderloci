898b1099c948120e682b213ffbbae2dd84d22c0e
Bikram Chatterjee <razorpeak@gmail.com>
2013-10-16 20:49:17 +0200

    added datetime_to_ora/1

--- src/dderloci.erl
+++ src/dderloci.erl
@@ -5,7 +5,8 @@
 %% API
 -export([
     exec/2,
-    inject_rowid/1
+    inject_rowid/1,
+    edatetime_to_ora/1
 ]).
 
 inject_rowid(Sql) ->
@@ -100,3 +101,13 @@ translate_datatype(Row, Cols) ->
         _ -> R
     end
     || {C,R} <- lists:zip(Cols, Row)].
+
+edatetime_to_ora({Meg,Mcr,Mil} = Now)
+    when is_integer(Meg)
+    andalso is_integer(Mcr)
+    andalso is_integer(Mil) ->
+    edatetime_to_ora(calendar:now_to_datetime(Now));
+edatetime_to_ora({{FullYear,Month,Day},{Hour,Minute,Second}}) ->
+    Century = (FullYear div 100) + 100,
+    Year = (FullYear rem 100) + 100,
+    << Century:8, Year:8, Month:8, Day:8, Hour:8, Minute:8, Second:8 >>.
